cbe9eefc714a881ec7b29d26ad2a290b5c8771f6
s35974-web <s35974@pjwstk.edu.pl>
2026-03-20 17:09:11 +0100

    Added: tests for Ksiazka, Main implementation

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+        Ksiazka ksiazka1 = new Ksiazka("Wiedzmin", "Kamil Wit", 320, true);
+        Ksiazka ksiazka2 = new Ksiazka("Lalka", "Boleslaw Prus", 680, true);
+        Ksiazka ksiazka3 = new Ksiazka("Hobbit", "Julia Tolkien", 310, false);
+
+        ksiazka1.wypiszInfo();
+        ksiazka2.wypiszInfo();
+        ksiazka3.wypiszInfo();
+
+        ksiazka1.wypozycz();
+        ksiazka1.wypiszInfo();
+
+        ksiazka1.zwroc();
+        ksiazka1.wypiszInfo();
+
+        ksiazka3.wypozycz();
+        ksiazka3.zwroc();
+        ksiazka3.wypiszInfo();
+    }
 }
